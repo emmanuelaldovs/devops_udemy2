f3995b2abf0519b1b83463480c49e45b70e6760c
Emmanuel Aldo Valerio Suárez <emmanuel.valerio@onetechnologyperu.com>
2022-02-07 22:44:33 -0500

    add teste modification 002

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -11,7 +11,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-				String meessage= "default message cambio test devops"
+				String meessage= "default message cambio test devops 002"
 				Assert.assertNotNull(meessage);
 	}
 
